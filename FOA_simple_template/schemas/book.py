cfdc665fb74830d19532b5414d006593b5bde66e
mudjaycker <tombutoyi@gmail.com>
2022-05-04 10:40:16 +0200

    BookBodyDeserializer

--- FOA_simple_template/schemas/book.py
+++ FOA_simple_template/schemas/book.py
@@ -1,7 +1,13 @@
+from typing import Optional
 from pydantic import BaseModel
 
 
 
 class BookBody(BaseModel):
+    age: int
+    author: str
+
+class BookBodyDeserializer(BaseModel):
+    id_: int
     age: int
     author: str
