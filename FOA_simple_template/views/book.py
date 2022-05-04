040ec0072515535e35b8b17f713d905bcd56a103
mudjaycker <tombutoyi@gmail.com>
2022-05-04 10:14:32 +0200

    creating views

---
+++ FOA_simple_template/views/book.py
@@ -0,0 +1,33 @@
+from regex import B
+from models.book import Book
+from schemas.book import BookBody
+
+from flask_openapi3 import APIBlueprint
+from flask_openapi3 import Tag
+
+tag = Tag(name="book", description="Some Book")
+
+api = APIBlueprint(
+    "/book",
+    __name__,
+    url_prefix="/api",
+    abp_tags=[tag],
+    # disable openapi UI
+    doc_ui=True,
+)
+
+
+@api.get("/book")
+def get_book():
+    """Get all Book
+    """
+    return {"books": [dict(BookBody(**b.to_json())) for b in Book.get_all()]}
+
+
+@api.post("/book")
+def create_book(body: BookBody):
+    "Post a book"
+    d = body.dict()
+    book = Book(**d)
+    book.save_to_db()
+    return {"code": 0, "message": "ok"}
