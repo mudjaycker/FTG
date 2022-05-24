854eab2f5fa249e5eb4d60d732e3a3d975c698a1
mudjaycker <tombutoyi@gmail.com>
2022-05-24 16:22:32 +0200

    create api directory

--- genereated_project/create.py
+++ genereated_project/create.py
@@ -9,4 +9,5 @@ views_path = path_from + "views/book.py"
 schemas_path = path_from + "schemas/book.py"
 models_path = path_from + "models/book.py"
 
-# 
+p = pathlib2.Path(path_to)
+p.mkdir(parents=True, exist_ok=True)
