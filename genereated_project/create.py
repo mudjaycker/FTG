24bcf80ed98943914ea240d1486035e968917dc7
mudjaycker <tombutoyi@gmail.com>
2022-05-24 16:21:06 +0200

    variables

--- genereated_project/create.py
+++ genereated_project/create.py
@@ -1,3 +1,12 @@
 import pathlib2
 import re
 from config import *
+
+
+path_to = lower_case
+path_from = "../FOA_simple_template/"
+views_path = path_from + "views/book.py"
+schemas_path = path_from + "schemas/book.py"
+models_path = path_from + "models/book.py"
+
+# 
